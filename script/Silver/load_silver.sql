5e7b4b09ba6048f66f5f91f4212cf3ebbfc25d5a
GunopsJo <133625031+GunopsJo@users.noreply.github.com>
2025-05-08 10:27:32 +0800

    Update load_silver.sql

--- script/Silver/load_silver.sql
+++ script/Silver/load_silver.sql
@@ -1,3 +1,12 @@
+/*
+
+Script Purpose:
+    This script creates tables in the 'silver' schema
+
+*/
+
+
+
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
 GO
