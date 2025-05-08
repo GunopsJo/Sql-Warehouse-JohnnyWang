3632eea496e03fad761d4c80d9c36f6e90684956
GunopsJo <133625031+GunopsJo@users.noreply.github.com>
2025-05-08 08:33:26 +0800

    Update load_bronze.sql

--- script/Bronze/load_bronze.sql
+++ script/Bronze/load_bronze.sql
@@ -7,69 +7,93 @@ Script Purpose:
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
 
-TRUNCATE TABLE bronze.crm_cust_info
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
-With(
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK 
-);
+DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+	BEGIN TRY
+	SET @batch_start_time = GETDATE();
+	TRUNCATE TABLE bronze.crm_cust_info
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	With(
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK 
+	);
 
-SELECT COUNT(*) FROM bronze.crm_cust_info
+	SELECT COUNT(*) FROM bronze.crm_cust_info
 
-TRUNCATE TABLE bronze.crm_prd_info;
+	TRUNCATE TABLE bronze.crm_prd_info;
 
-		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
-		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
-
-
-
-TRUNCATE TABLE bronze.crm_sales_details;
-		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
-		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
+			PRINT '>> Inserting Data Into: bronze.crm_prd_info';
+			BULK INSERT bronze.crm_prd_info
+			FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+			WITH (
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			);
 
 
-TRUNCATE TABLE bronze.erp_loc_a101;
-		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
-		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
+
+	TRUNCATE TABLE bronze.crm_sales_details;
+			PRINT '>> Inserting Data Into: bronze.crm_sales_details';
+			BULK INSERT bronze.crm_sales_details
+			FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+			WITH (
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			);
 
 
-		TRUNCATE TABLE bronze.erp_cust_az12;
-		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
-		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
+	TRUNCATE TABLE bronze.erp_loc_a101;
+			PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
+			BULK INSERT bronze.erp_loc_a101
+			FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+			WITH (
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			);
 
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
-		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
-		WITH (
-			FIRSTROW = 2,
-			FIELDTERMINATOR = ',',
-			TABLOCK
-		);
+
+			TRUNCATE TABLE bronze.erp_cust_az12;
+			PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+			BULK INSERT bronze.erp_cust_az12
+			FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+			WITH (
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			);
+
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+			PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+			BULK INSERT bronze.erp_px_cat_g1v2
+			FROM 'C:\Users\Jo\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+			WITH (
+				FIRSTROW = 2,
+				FIELDTERMINATOR = ',',
+				TABLOCK
+			);
+
+			SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
+
+		SET @batch_end_time = GETDATE();
+		PRINT '=========================================='
+		PRINT 'Loading Bronze Layer is Completed';
+        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+		PRINT '=========================================='
+			END TRY
+
+	BEGIN CATCH
+		PRINT '=========================================='
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+		PRINT 'Error Message' + ERROR_MESSAGE();
+		PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '=========================================='
+	END CATCH
+
 END
